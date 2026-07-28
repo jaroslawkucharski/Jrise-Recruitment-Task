968e49c21d3429e617c99d5cacb191f27e3db2c1
Jarosław <jaroslaw.kucharski@landingi.com>
2026-07-28 15:06:38 +0200

    code fixes

--- components/atoms/Text/Text.tsx
+++ components/atoms/Text/Text.tsx
@@ -1,23 +1,23 @@
 import clsx from "clsx";
 import type { ReactNode } from "react";
 
-type TextSize = 12 | 14 | 16;
-type TextWeight = 400 | 500 | 600 | 700;
+type TextSizeTypes = 12 | 14 | 16;
+type TextWeightTypes = 400 | 500 | 600 | 700;
 
 type TextProps = {
   className?: string;
   children: ReactNode;
-  size?: TextSize;
-  weight?: TextWeight;
+  size?: TextSizeTypes;
+  weight?: TextWeightTypes;
 };
 
-const sizeClassNames: Record<TextSize, string> = {
+const sizeClassNames: Record<TextSizeTypes, string> = {
   12: "text-[12px]",
   14: "text-[14px]",
   16: "text-[16px]",
 };
 
-const weightClassNames: Record<TextWeight, string> = {
+const weightClassNames: Record<TextWeightTypes, string> = {
   400: "font-normal",
   500: "font-medium",
   600: "font-semibold",
