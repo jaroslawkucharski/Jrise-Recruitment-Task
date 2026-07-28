56309e11c0ab878ecbdbd5fb24aedb1200e83b12
Jarosław <jaroslaw.kucharski@landingi.com>
2026-07-28 17:01:08 +0200

    crate sectionSecondSlides data

---
+++ data/slides.ts
@@ -0,0 +1,45 @@
+import { SlidesSectionProps } from "@/components/organisms/SlidesSection/SlidesSection";
+
+export const sectionSecondSlides: SlidesSectionProps = {
+  id: "jakpracuje",
+  sectionName: "Jak pracuje",
+  ariaLabel: "Jak pracuje",
+  slides: [
+    {
+      title: "process_slide_1_title",
+      description: "process_slide_1_description",
+      imageAlt: "process_slide_1_title",
+      imageSrc: "/section_02_01.webp",
+      colorImageSrc: "/section_02_01_color.webp",
+      isImageLoadingEager: true,
+    },
+    {
+      title: "process_slide_2_title",
+      description: "process_slide_2_description",
+      imageAlt: "process_slide_2_title",
+      imageSrc: "/section_02_02.webp",
+      colorImageSrc: "/section_02_02_color.webp",
+    },
+    {
+      title: "process_slide_3_title",
+      description: "process_slide_3_description",
+      imageAlt: "process_slide_3_title",
+      imageSrc: "/section_02_03.webp",
+      colorImageSrc: "/section_02_03_color.webp",
+    },
+    {
+      title: "process_slide_4_title",
+      description: "process_slide_4_description",
+      imageAlt: "process_slide_4_title",
+      imageSrc: "/section_02_04.webp",
+      colorImageSrc: "/section_02_04_color.webp",
+    },
+    {
+      title: "process_slide_5_title",
+      description: "process_slide_5_description",
+      imageAlt: "process_slide_5_title",
+      imageSrc: "/section_02_05.webp",
+      colorImageSrc: "/section_02_05_color.webp",
+    },
+  ],
+};
