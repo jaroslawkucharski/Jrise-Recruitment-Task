97052c774ed78db068e5389ace5740c1784b5a18
Jarosław <jaroslaw.kucharski@landingi.com>
2026-07-31 15:04:05 +0200

    code fixes

--- components/organisms/Header/Header.tsx
+++ components/organisms/Header/Header.tsx
@@ -8,7 +8,7 @@ export async function Header() {
   const t = await getAppTranslations();
 
   return (
-    <header className="sticky top-0 z-10 w-full bg-neutral-hover/70 shadow-xl/30 backdrop-blur-md">
+    <header className="sticky top-0 z-20 w-full bg-neutral-hover/70 shadow-xl/30 backdrop-blur-md">
       <div className="mx-auto flex h-18 w-full max-w-300 items-center justify-between gap-6 px-4 sm:px-6 md:px-12 xl:px-0">
         <Link href={"/"} aria-label={t("header_homeAria")}>
           <Logo />
